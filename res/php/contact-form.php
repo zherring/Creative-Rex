4bd9c52a6ae499fd69dffef57e28b40440090d34
zherring <z.herring@gmail.com>
2016-10-21 10:31:37 -0600

    contact form

---
+++ res/php/contact-form.php
@@ -0,0 +1,57 @@
+<?php
+    //
+    // $to = strip_tags("z.herring@gmail.com");
+    // $first_name = strip_tags($_POST['first_name']);
+    // $last_name = strip_tags($_POST['last_name']);
+    // $phone = strip_tags($_POST['phone']);
+    // $email = strip_tags($_POST['email']);
+    // $content = strip_tags($_POST['content']);
+    //
+    // $fields = array();
+    // $fields{"first_name"} = $first_name;
+    // $fields{"last_name"} = $last_name;
+    // $fields{"email"} = $email;
+    // $fields{"phone"} = $phone;
+    // $fields{"message"} = $content;
+    //
+    // $headers = "From: $from;";
+    // $subject = "Contact Form Result from " . $first_name . " " . $last_name;
+    //
+    // $body = "Here is what was sent:\n\n";
+    //         foreach($fields as $a => $b) {
+    //             $body .= sprintf("%20s: %s\n",$b,$fields[$a]);
+    //             }
+    //
+    // $send = mail($to, $subject, $body, $headers);
+    //
+    // header("Location: ../../contacted.html#contact");
+
+    // var_dump($_POST);
+
+
+    var_dump($_POST);
+
+
+    // var_dump($_REQUEST);
+
+    // $fields = array();
+    //     $fields["to"] = strip_tags("z.herring@gmail.com");
+    //     $fields["first_name"] = strip_tags($_REQUEST['first_name']);
+    //     $fields["last_name"] = strip_tags($_REQUEST['last_name']);
+    //     $fields["email"] = strip_tags($_REQUEST['email']);
+    //     $fields["phone"] = strip_tags($_REQUEST['phone']);
+    //     $fields["message"] = strip_tags($_REQUEST['content']);
+    //
+    //     $headers = "From: " . $fields["email"];
+    //     $subject = "Contact Form Result from " . $fields['first_name'] . " " . $fields['last_name'];
+    //     // var_dump($_REQUEST);
+    //
+    //     $body = "Here is what was sent:\n\n";
+    //     foreach($fields as $a => $b) {
+    //         $body .= sprintf("%20s: %s\n",$a,$b);
+    //     }
+    //
+    //     $send = mail($fields["to"], $subject, $body, $headers);
+    //
+    //     header("location: /contacted.html#contact");
+?>
